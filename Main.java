4d9303bb123c87aa58e29e1baf2ecceaa658af99
Ben Hodges <71901668+Been-H@users.noreply.github.com>
2021-04-15 10:00:37 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ import java.awt.*;
 public class Main {
 
     public static void main(String[] args) {
+        //an example of using linear regression to predict hunting success in chimpanzee hunting parties depending on the number of chimps in the party
         LinearRegression linreg = new LinearRegression();
         double[] xData = {1,2,3,4,5,6,7,8};
         double[] yData = {30, 45, 51, 57, 60, 65, 70, 71};
